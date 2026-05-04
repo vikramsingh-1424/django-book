7feaa99ff5bc05c1b7ef0af3d01e719f73bdfcee
Vikram singh <vikaramsinghsikarwar@gmail.com>
2026-05-04 12:27:53 +0530

    fixed deployment settings

---
+++ djnago-bookmyseat/movies/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+urlpatterns=[
+    path('',views.movie_list,name='movie_list'),
+    path('<int:movie_id>/theaters',views.theater_list,name='theater_list'),
+    path('theater/<int:theater_id>/seats/book/',views.book_seats,name='book_seats'),
+]
